92667007b6e99d4f3bc75265f3a9cbf15534e10c
Yaodong Zhao <git@yaodong.dev>
2024-09-30 20:34:32 -0700

    Add tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('postcss-nesting'),
-    require('autoprefixer'),
-  ],
-}
+  plugins: {
+    "postcss-import": {},
+    "tailwindcss/nesting": "postcss-nesting",
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
